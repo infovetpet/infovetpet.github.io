3b684d17feab9ce8b8d8f0d704ea31da750b58e3
Lks <wapiorder@gmail.com>
2025-09-10 11:32:07 -0300

    nueva version

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square, pawOutline, medkitOutline, logoWhatsapp, footballOutline, fastFoodOutline } from 'ionicons/icons';
+import { triangle, ellipse, square, pawOutline, medkitOutline, logoWhatsapp, basketballOutline, fastFoodOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -13,6 +13,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square, pawOutline, medkitOutline, logoWhatsapp, footballOutline, fastFoodOutline });
+    addIcons({ triangle, ellipse, square, pawOutline, medkitOutline, logoWhatsapp, basketballOutline, fastFoodOutline });
   }
 }
